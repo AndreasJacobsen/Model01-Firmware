a9f8bc5950a1b23cafffbf7ce7a2199649c3cce9
Andreas Jacobsen <andreas1300@gmail.com>
2018-03-11 22:30:36 +0100

    added support for æåø and gaming layer with bcksp replaced by space key

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -73,12 +73,13 @@
   * a macro key is pressed.
   */
 
-enum { MACRO_VERSION_INFO,
-       MACRO_ANY,
-       R_AE,
-       R_OE,
-       R_AA
-     };
+enum {
+  MACRO_VERSION_INFO,
+  MACRO_ANY,
+  COMPOSE_AA,
+  COMPOSE_AE,
+  COMPOSE_OE,
+};
 
 
 
@@ -124,7 +125,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { QWERTY, NUMPAD, FUNCTION }; // layers
+enum { QWERTY, BOKMAL, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -134,19 +135,35 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (Key_Escape,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   LockLayer(BOKMAL),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, ShiftToLayer(BOKMAL),
    ShiftToLayer(FUNCTION)),
+
+
+  [BOKMAL] =  KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, Key_Spacebar, ___, ___,
+   ___,
+
+   LockLayer(BOKMAL), ___, ___, ___, ___,           ___,           ___,
+   ___,               ___, ___, ___, ___,           M(COMPOSE_AA), ___,
+                      ___, ___, ___, M(COMPOSE_OE), M(COMPOSE_AE), ___,
+   ___,               ___, ___, ___, ___,           ___,           ___,
+   ___, ___, ___, ___,
+   ___),
 
 
   [NUMPAD] =  KEYMAP_STACKED
@@ -163,6 +180,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
    ___, ___, ___, ___,
    ___),
+
 
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
@@ -213,6 +231,32 @@ static void anyKeyMacro(uint8_t keyState) {
     kaleidoscope::hid::pressKey(lastKey);
 }
 
+void composeTapKey(Key key, bool shift_state = false) {
+  if (shift_state && key.keyCode >= Key_A.keyCode && key.keyCode <= Key_Z.keyCode) {
+    kaleidoscope::hid::pressKey(Key_LeftShift);
+  }
+  kaleidoscope::hid::pressKey(key);
+  kaleidoscope::hid::sendKeyboardReport();
+  kaleidoscope::hid::releaseKey(key);
+  kaleidoscope::hid::sendKeyboardReport();
+}
+
+void compose(Key a, Key b, bool shift_both = false) {
+  bool lshift = kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift);
+  bool rshift = kaleidoscope::hid::wasModifierKeyActive(Key_RightShift);
+
+  kaleidoscope::hid::releaseKey(Key_LeftShift);
+  kaleidoscope::hid::releaseKey(Key_RightShift);
+
+  composeTapKey(Key_RightControl);
+  composeTapKey(a, shift_both && (lshift || rshift));
+  composeTapKey(b, lshift || rshift);
+
+  if (lshift)
+    kaleidoscope::hid::pressKey(Key_LeftShift);
+  if (rshift)
+    kaleidoscope::hid::pressKey(Key_RightShift);
+}
 
 /** macroAction dispatches keymap events that are tied to a macro
     to that macro. It takes two uint8_t parameters.
@@ -226,28 +270,34 @@ static void anyKeyMacro(uint8_t keyState) {
 
  */
 
-
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
 
-  case MACRO_VERSION_INFO:
-    versionInfoMacro(keyState);
-    break;
-
-  case MACRO_ANY:
-    anyKeyMacro(keyState);
-    break;
-
-   case R_AE:
-      compose2(Key_A, true, Key_E, true, keyState);
+    case MACRO_VERSION_INFO:
+      versionInfoMacro(keyState);
       break;
-   case R_OE: 
-      compose2(Key_Slash, false, Key_O, true, keyState);
-      break;
-   case R_AA:
-      compose2(Key_O, false, Key_A, true, keyState);
-      break; 
 
+    case MACRO_ANY:
+      anyKeyMacro(keyState);
+      break;
+
+    case COMPOSE_AA:
+      if (keyToggledOn(keyState)) {
+        compose(Key_O, Key_A);
+      }
+      break;
+
+    case COMPOSE_AE:
+      if (keyToggledOn(keyState)) {
+        compose(Key_A, Key_E, true);
+      }
+      break;
+
+    case COMPOSE_OE:
+      if (keyToggledOn(keyState)) {
+        compose(Key_Slash, Key_O, true);
+      }
+      break;
   }
   return MACRO_NONE;
 }
@@ -350,7 +400,7 @@ void setup() {
 
     // The macros plugin adds support for macros
     &Macros,
- 
+
     // The MouseKeys plugin lets you add keys to your keymap which move the mouse.
     &MouseKeys,
 
