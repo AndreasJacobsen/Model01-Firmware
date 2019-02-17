98d069a68fd61571118925351149eacd02a2742e
Andreas Jacobsen <andreas1300@gmail.com>
2019-02-17 01:34:13 +0100

    Changed shift to layer to lock layer for gaming

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -178,7 +178,7 @@ KEYMAPS(
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   ShiftToLayer(GAMING),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+   LockLayer(GAMING),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
@@ -261,7 +261,7 @@ KEYMAPS(
    ___),
 
      [GAMING] =  KEYMAP_STACKED
-  (ShiftToLayer(PRIMARY), ___, ___, ___, ___, ___,   ___,
+  (LockLayer(PRIMARY), ___, ___, ___, ___, ___,   ___,
    ___, ___, ___, ___, ___, ___, ___,
    Key_Tab, ___, ___, ___, ___, ___,
    Key_LeftShift, ___, ___, ___, ___, ___, ___,
